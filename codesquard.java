8a2fb6cd243d1a82d5ad52d200fccf2949bf1cf8
young hun <thddudrb79@naver.com>
2020-12-11 20:12:37 +0900

    변수 생성 및 input() 함수 추가
    * 문자열, 숫자, 방향을 담을 변수 생성
    * 입력 처리 담당하는 input 함수 추가

--- codesquard.java
+++ codesquard.java
@@ -1,9 +1,20 @@
-
+import java.util.*;
 public class codesquard {
-
+	static Scanner sc = new Scanner(System.in);
+	static String str; // 문자열 
+	static int cnt; // 숫자
+	static char dir; // 방향 : 왼쪽 or 오른쪽 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		input();
 
 	}
 
+	public static void input() {
+		System.out.print("> ");
+		String str = sc.next();
+		int cnt = sc.nextInt();
+		char dir = sc.next().charAt(0);
+	}
+	
 }
