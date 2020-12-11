42a1a48e1ba2d151eac9290452ece46315627615
young hun <thddudrb79@naver.com>
2020-12-11 13:32:34 +0900

    push 함수 추가
    * 입력된 방향으로 알파벳을 밀고 그 결과값을 문자열 형태로 반환하는 push 함수 구현
    * 입력으로 가능한 8방향(U,U',R,R',L,L',B,B')을 push함수 하나로 모두 대응할 수 있음

--- codesquard.java
+++ codesquard.java
@@ -1,9 +1,24 @@
-
+import java.util.*;
 public class codesquard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+	}
+	
+	public static String push(String input, boolean direction) { // 입력된 방향대로 미는 동작 수행 
+		Queue<Character> q = new LinkedList<Character>();
+		for(int i = 0; i < input.length(); i++)
+			q.add(input.charAt(i)); 
+		
+		q.add(q.remove()); // 미는 방향이 왼쪽 = 큐에서 1번만 빼기
+		if(!direction) // 미는 방향이 반대 = 큐에서 2번 빼기 
+			q.add(q.remove());
+		
+		String result = ""; 
+		while(!q.isEmpty())  
+			result += q.remove(); // 문자열에 결과값을 담아 반환
+		return result;
 	}
 
 }
