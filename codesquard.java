77e23a0621eaea721f1eb43198ff8bbeb0c2eb18
young hun <thddudrb79@naver.com>
2020-12-11 15:00:18 +0900

    print() 함수 추가
    * 현재 큐브의 상태를 출력하는 print() 함수 추가

--- codesquard.java
+++ codesquard.java
@@ -16,17 +16,17 @@ public class codesquard {
 	public static void push(char[][] board, boolean direction, char location) { // 입력된 방향대로 미는 동작 수행
 		Queue<Character> q = new LinkedList<Character>();
 		if (location == 'U') {
-			for(int i = 0; i < 3; i++)
+			for(int i = 0; i < 3; i++) // 해당 문자 순서대로 큐에 담기
 				q.add(board[0][i]);
 			
-			q.add(q.remove());
-			if(!direction)
+			q.add(q.remove());  // 뒤로 한 번 밀기
+			if(!direction)   // 역방향일 경우 뒤로 두 번 밀기
 				q.add(q.remove());
 			
-			for (int i = 0; i < 3; i++)
+			for (int i = 0; i < 3; i++) // 큐에 담은 순서대로 보드에 값 갱신하기
 				board[0][i] = q.remove();
 		}
-		if (location == 'R') {
+		if (location == 'R') {      
 			for(int i = 0; i < 3; i++)
 				q.add(board[i][2]);
 			
@@ -72,6 +72,15 @@ public class codesquard {
 		board[2][0] = 'G';
 		board[2][1] = 'B';
 		board[2][2] = 'B';
+	}
+	
+	public static void print(char[][] board) { // 현재 큐브 출력
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	
